567448ff0b46847c0096177266b9f1d2a95a51a3
Lucas Emanuel Santana dos Santos <lucasemanuel2077@gmail.com>
2022-02-15 12:03:36 -0300

    upload

--- pages/index.js
+++ pages/index.js
@@ -74,12 +74,14 @@ function HomePage(){
                 <meta name="robots" content="index,follow" />
                 <meta name="theme-color" content="#97d7d7" />
                 <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+                <meta property="og:type" content="website" />
                 <meta property="og:title" content="Projeto pessoal - Rick and Morty API" />
                 <meta property="og:site_name" content="Rick and Morty" />
                 <meta property="og:description" content="Projeto criado a partir da Rick and Morty API." />
                 <meta property="og:url" content="https://rick-and-morty-api-phi-eight.vercel.app/" />
                 <meta property="og:image" content="https://rick-and-morty-api-phi-eight.vercel.app/images/og-img.jpg" />
                 <meta property="og:image:type" content="image/jpeg" />
+                <meta property="fb:app_id" content="2166958726772168" />
                 <link rel="icon" href="#" type="image/x-icon" />
             </Head>
             <main>
